26f4bbaac3eed5141390bcd2af41c9a738783fd6
Unknown <jahidcsensu@gmail.com>
2019-02-28 13:12:33 +0600

    Java Script and Css

---
+++ javaScript Practise/app.js
@@ -0,0 +1,15 @@
+//var x = 9;
+// console.log("hello how are you");
+// console.log(x);
+// document.write("Hello");
+// document.write(x);
+// arr = [1, 3, 4];
+// console.log(arr);
+// document.write(arr);
+//window.alert(x);
+// document.getElementById("demo").innerHTML="hello";
+// document.getElementById("hh").innerHTML=(x*x);
+// document.getElementById("hh").innerHTML=Date();
+// console.log(Date());
+// document.write(Date());
+// window.alert(Date());
